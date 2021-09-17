c4104c9efb5d2fc1b7a3668669b8b3376730d735
Ancelmo Luiz <44513782+AncelmoLuizHotmail@users.noreply.github.com>
2021-09-16 22:14:40 -0400

    Adicionar arquivos de projeto.

---
+++ src/Escola.Web/Web/Controllers/AlunoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Index()
+        {
+            Aluno aluno = new Aluno();
+
+            aluno.Nome = "Eduardo Henrique";
+            aluno.Idade = 21;
+            aluno.Matricula = "123456";
+            aluno.Cpf = "123.456.789-10";
+
+            return View(aluno);
+        }
+    }
+}
